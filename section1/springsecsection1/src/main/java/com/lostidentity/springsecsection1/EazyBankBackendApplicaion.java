72c00f238da5fd0dcf907f3a599948839805b954
Ajay Patidar <patidarajay769@gmail.com>
2025-04-27 11:24:40 +0530

    learned basic authentication with default cred's and custom cred's provided in properties

---
+++ section1/springsecsection1/src/main/java/com/lostidentity/springsecsection1/EazyBankBackendApplicaion.java
@@ -0,0 +1,14 @@
+package com.lostidentity.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplicaion {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EazyBankBackendApplicaion.class, args);
+	}
+
+}

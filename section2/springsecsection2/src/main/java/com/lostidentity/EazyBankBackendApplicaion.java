a2284e880f8bcc765caf9178f98e03e057542af5
Ajay Patidar <patidarajay769@gmail.com>
2025-04-27 19:59:38 +0530

    Learned how to customize spring security in our appliction, how to disable form login and basic authentication and testing the api's from postman

---
+++ section2/springsecsection2/src/main/java/com/lostidentity/EazyBankBackendApplicaion.java
@@ -0,0 +1,14 @@
+package com.lostidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplicaion {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EazyBankBackendApplicaion.class, args);
+	}
+
+}

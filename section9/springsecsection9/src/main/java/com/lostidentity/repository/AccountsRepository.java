305b1a3bbc517ec4f155f9d14c4be2569d9b1320
Ajay Patidar <patidarajay769@gmail.com>
2025-06-04 22:56:35 +0530

    IN this section we have learned about authorization and how can we configure authz into our service

---
+++ section9/springsecsection9/src/main/java/com/lostidentity/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package com.lostidentity.repository;
+
+
+import com.lostidentity.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(long customerId);
+
+}
